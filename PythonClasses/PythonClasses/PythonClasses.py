87323a101ba4a8b2ca21e95f4129afc10b6f5de0
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-03-23 23:48:45 -0700

    Add files via upload

---
+++ PythonClasses/PythonClasses/PythonClasses.py
@@ -0,0 +1,44 @@
+import math
+class Rectangle:
+    def __init__(self, width, height): 
+        self.rArea = width * height
+    def pArea(self):
+        print("The area of a Rectangle is ",self.rArea)
+class Triangle:
+    def __init__(self, width, base): 
+        self.tArea = width * base/2
+    def pArea(self):
+        print("The area of a Triangle is ",self.tArea)
+class Square:
+    def __init__(self, side): 
+        self.sArea = pow(side, 2)
+        self.bArea = 6*(pow(side,2))
+    def pArea(self):
+        print("The area of a Square is ", self.sArea)
+        print("The area of a Cube is ", self.bArea)
+class Circle:
+    def __init__(self, radius): 
+        self.cArea = round( (math.pi*pow(radius,2)),2)
+    def pArea(self):
+        print("The area of a Circle is ",self.cArea)
+
+
+# enter retangle
+r1 = Rectangle(4, 5)
+# print rectangle
+r1.pArea()
+
+# enter Triangle
+r1 = Triangle(4, 5)
+# print rectangle
+r1.pArea()
+
+# enter Square
+r1 = Square(5)
+# print rectangle
+r1.pArea()
+
+# enter Square
+r1 = Circle(5)
+# print rectangle
+r1.pArea()
